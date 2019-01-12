eeda248eaadebc32fa7402bb4ffc4a006ef701a3
reaganiwadha <reaganiwadha@gmail.com>
2019-01-12 20:09:39 +0700

    t i m e i s k e y

---
+++ wp-content/themes/fashiontheme/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link href="https://fonts.googleapis.com/css?family=Abhaya+Libre:400,700" rel="stylesheet">
+        <title>FWEEK</title>
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css">
+
+	    <?php wp_head();?>
+    </head>
+    <body>
+        <nav class="navbar">
+            <h1 class="title">FWEEK</h1>
+        </nav>
+    </body>
+</html>
