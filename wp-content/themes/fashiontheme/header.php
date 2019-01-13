b4ddce4da18267c2f9bf9196e425dde74be080d2
reaganiwadha <reaganiwadha@gmail.com>
2019-01-13 20:55:09 +0700

    saya akui, saya pemalas

--- wp-content/themes/fashiontheme/header.php
+++ wp-content/themes/fashiontheme/header.php
@@ -5,12 +5,21 @@
         <title>FWEEK</title>
 	    <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css">
-
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	    <?php wp_head();?>
     </head>
     <body>
         <nav class="navbar">
+            <i class="material-icons menu-nav">menu</i>
             <h1 class="title">FWEEK</h1>
         </nav>
+        <div class="row-content-1">
+            <div class="c1">
+                <h1>Global Fashion Week 2017</h1>
+            </div>
+            <div class="c2">
+                <img src="http://www.fillmurray.com/640/360">
+            </div>
+        </div>
     </body>
 </html>
